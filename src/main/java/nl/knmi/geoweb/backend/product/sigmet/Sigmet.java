c389ecc53a5ac937278af5eba4975d3c9bda97a4
Ernst de Vreede <ernstdevreede@xs4all.nl>
2018-06-27 09:29:52 +0000

    Code cleanup

--- src/main/java/nl/knmi/geoweb/backend/product/sigmet/Sigmet.java
+++ src/main/java/nl/knmi/geoweb/backend/product/sigmet/Sigmet.java
@@ -371,8 +371,6 @@ public class Sigmet {
 		this.status=SigmetStatus.PRODUCTION;
 	}
 
-	static String testGeoJson="{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[4.44963571205923,52.75852934878266],[1.4462013467168233,52.00458561642831],[5.342222631879865,50.69927379063084],[7.754619712476178,50.59854892065259],[8.731640530117685,52.3196364467871],[8.695454573908739,53.50720041878871],[6.847813968390116,54.08633053026368],[3.086939481359807,53.90252679590722]]]},\"properties\":{\"prop0\":\"value0\",\"prop1\":{\"this\":\"that\"}}}]}";
-
 	public static Sigmet getSigmetFromFile(ObjectMapper om, File f) throws JsonParseException, JsonMappingException, IOException {
 		Sigmet sm=om.readValue(f, Sigmet.class);
 		Debug.println("Sigmet from "+f.getName());
@@ -581,40 +579,6 @@ public class Sigmet {
 		return sb.toString();
 	}
 
-	//	public static void main(String args[]) throws IOException {
-	//		Sigmet sm=new Sigmet("AMSTERDAM FIR", "EHAA", "EHDB", "abcd");
-	//		sm.setPhenomenon(Phenomenon.getPhenomenon("OBSC_TS"));
-	//		sm.setValiddate(new Date(117,2,13,16,0));
-	//		Debug.println(sm.getValiddate().toString());
-	//		sm.setChange(SigmetChange.NC);
-	//		sm.setGeoFromString(testGeoJson);
-	//		Debug.println(sm.getPhenomenon().toString());
-	//		
-	//		SigmetStore store=new SigmetStore("/tmp");
-	////		store.storeSigmet(sm);
-	//		
-	//		ObjectMapper objectMapper = new ObjectMapper();
-	//		// DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-	//		// objectMapper.setDateFormat(df);
-	//		try{
-	//			String v = objectMapper.writeValueAsString(sm);
-	//			JSONObject j = (JSONObject) new JSONTokener(v).nextValue();
-	//			Debug.println(j.get("issuedate").toString());
-	//		}catch(JsonProcessingException e){
-	//			e.printStackTrace();
-	//		} catch (JSONException e) {
-	//			// TODO Auto-generated catch block
-	//			e.printStackTrace();
-	//		}
-	//		
-	////		System.err.println(sm);
-	////		
-	////		for (int i=0; i<1; i++) {
-	////			sm=Sigmet.getRandomSigmet();
-	////			store.storeSigmet(sm);
-	////			System.err.println(i+": "+sm);
-	////		}
-	//	}
 	private static String START="start";
 	private static String END="end";
 	private static String INTERSECTION="intersection";
