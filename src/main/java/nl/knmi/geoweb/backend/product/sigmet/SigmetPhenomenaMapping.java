20baac431144cb9d7d009bd5da6129095a4e6512
maartenplieger <maartenplieger@gmail.com>
2018-10-03 10:02:58 +0000

    [SIGMET] Updatedsigmets/getsigmetphenomena with volcanic ash

--- src/main/java/nl/knmi/geoweb/backend/product/sigmet/SigmetPhenomenaMapping.java
+++ src/main/java/nl/knmi/geoweb/backend/product/sigmet/SigmetPhenomenaMapping.java
@@ -76,6 +76,8 @@ public class SigmetPhenomenaMapping {
 		phenomena.add(phen);
 		phen=new SigmetPhenomenon("Severe Mountainwaves", "SEV_MTW", "sigmet_layer_SEV_MTW");
 		phenomena.add(phen);
+		phen=new SigmetPhenomenon("Volcanic Ash eruption", "VA_ERUPTION", null);
+		phenomena.add(phen);
 	}
 
 }
